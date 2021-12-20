87147cf986967681b25db06644dc9e00f9b73223
mertvnl <41839620+mertvnl@users.noreply.github.com>
2021-12-20 21:24:49 +0300

    added: 3D assets and camera controller

---
+++ Assets/DunkBall/Scripts/Controllers/CameraController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DunkBall.Camera
+{
+    public class CameraController : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] private Transform basket;
+        [SerializeField] private Transform ball;
+
+        [Header("Camera Settings")]
+        [SerializeField] private Vector3 followOffset = Vector3.zero;
+
+
+        private void LateUpdate()
+        {
+            HandleRotation();
+            HandlePosition();
+        }
+
+        private void HandleRotation()
+        {
+            transform.LookAt(basket);
+        }
+
+        private void HandlePosition()
+        {
+            Vector3 targetPosition = ball.position + followOffset;
+            //Lock Y position of camera
+            targetPosition.y = transform.position.y;
+            transform.position = targetPosition;
+        }
+    }
+}
+
